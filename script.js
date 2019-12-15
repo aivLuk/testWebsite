b354304fcbeefb6c26bd8cefd193d8a48602faa9
Aivaras <lukauskasaivaras@gmail.com>
2019-12-15 12:35:52 +0100

    Final

--- script.js
+++ script.js
@@ -3,23 +3,24 @@ async function getResults() {
         .then(result => {
             return result.json();
         })
+        .catch(error => allert(error));
     const data = await getPics(res)
 
 }
 
 function getPics(arr) {
     for (let i = 0; i < arr.length; i++) {
-        let pic = document.querySelector(".left");
+        let leftPic = document.querySelector(".left_container");
         let img = document.createElement("img");
         img.setAttribute("class", "styling")
         img.src = arr[i].download_url;
-        pic.appendChild(img)
+        leftPic.appendChild(img)
 
         img.addEventListener("click", function () {
-            let right = document.querySelector("#rightpic");
-            let rightImg = document.querySelector("#main");
+            let rightContainer = document.querySelector("#rightPicDiv");
+            let rightImg = document.querySelector("#mainImg");
             rightImg.src = arr[i].download_url;
-            right.style.display = "block";
+            rightContainer.style.display = "block";
             let info = document.querySelector("h3");
             info.textContent = `Author: ${arr[i].author}, Width: ${arr[i].width}, Height: ${arr[i].height}`;
 
